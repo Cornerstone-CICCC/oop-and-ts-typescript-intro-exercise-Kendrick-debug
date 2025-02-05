37cc13e171df82e14d2a0e65603a3a2ef5baeb97
Philip Yaw Boahene <boahenep22@gmail.com>
2025-02-04 19:48:10 -0800

    typescript exe done

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,18 +1,14 @@
+"use strict";
 // 3. Create a function named `celsiusToFahrenheit` that takes a number (temperature in Celsius).
 //    The function should return the equivalent temperature in Fahrenheit.
 //    Formula: Fahrenheit = (Celsius * 9/5) + 32
 //    Ensure the parameter and return type are typed correctly.
-
-function celsiusToFahrenheit(celsius: number):number {
-    return (celsius * 9/5) + 32;
-
-
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
     // formula for converting celsius to fahrenheit
     // is (celsius * 9/5) + 32
     //so basically yh
-
 }
-
 // Expected output:
-console.log(celsiusToFahrenheit(0));  // 32
+console.log(celsiusToFahrenheit(0)); // 32
 console.log(celsiusToFahrenheit(25)); // 77
